a7e141948b7b2c143d6bca7b80eb54ae1f1a9f34
mariiaglushak <90639229+mariiaglushak@users.noreply.github.com>
2024-03-07 18:28:29 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-project">
+    <BrowserRouter basename="test-project">
       <App />
     </BrowserRouter>
   </React.StrictMode>
